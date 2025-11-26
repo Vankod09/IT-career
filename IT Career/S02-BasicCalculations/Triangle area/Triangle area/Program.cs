7c38f95a13d18b3091019d24df51c818eb4e8bdd
Vanko09 <vanko09@abv.bg>
2025-11-26 21:39:59 +0200

    IT Career

---
+++ IT Career/S02-BasicCalculations/Triangle area/Triangle area/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double h = double.Parse(Console.ReadLine());
+        double area = a * h / 2;
+        area = Math.Round(area, 2);
+        Console.WriteLine($"Triangle area = {area}");
+    }
+}
