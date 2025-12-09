b5db32947f367822b1db033ce096c93f5e7d0c60
Vanko09 <vankod09@gmail.com>
2025-12-09 12:41:13 +0200

    IT Career

---
+++ IT Career/M01-IntroToPrograming/S04-Loops/P23-TheLCD(NOD)/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int NOD;
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        {
+            while (b != 0)
+            {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+            Console.WriteLine(a);
+        }
+
+    }
+}
---
+++ IT Career/M01-IntroToPrograming/S04-Loops/P24-Factoriel/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int a = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            a = a * i;
+        }
+        Console.WriteLine(a);
+    }
+}
