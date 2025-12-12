2bcf42827400e9e2ce63fdcf52ab2273fdaaf5d9
Vanko09 <vankod09@gmail.com>
2025-12-12 17:07:00 +0200

    IT Career

---
+++ IT Career/M01-IntroToPrograming/S04-Loops/P26-EvenNumberWithCheck/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        for (; ; )
+        {
+
+            Console.Write("Enter even number: ");
+            string a = Console.ReadLine();
+            int n;
+
+            if (!int.TryParse(a, out n))
+            {
+                Console.WriteLine("Invalid number!");
+                continue;
+            }
+            if (n % 2 != 0)
+            {
+                Console.WriteLine("The number is not even.");
+            }
+            else if (n % 2 == 0)
+            {
+                Console.WriteLine($"{n}");
+                break;
+            }
+        }
+    }
+}
---
+++ IT Career/M01-IntroToPrograming/S04-Loops/P27-FibonachiNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int f0 = 1;
+        int f1 = 1;
+
+        if (n < 2)
+        {
+            Console.WriteLine(1);
+        }
+        else
+        {
+            int fibNum = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                fibNum = f0 + f1;
+                f0 = f1;
+                f1 = fibNum;
+            }
+            Console.WriteLine(fibNum);
+        }
+    }
+}
